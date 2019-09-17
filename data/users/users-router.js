460e65cb53e791c6a842518ac41722912d193868
TheHaunted <kevin_lopez@live.com>
2019-09-17 16:50:29 -0500

    MVP part 2

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -5,7 +5,7 @@ const Users = require('./users-model.js');
 
 const router = express.Router();
 
-router.get('/', auth, (req, res) => {
+router.get('/', isLoggedIn, (req, res) => {
     Users.getUsers()
         .then(users => res.status(200).json(users))
         .catch(err => {
@@ -14,9 +14,9 @@ router.get('/', auth, (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/register', (req, res) => {
     const { username, password } = req.body;
-    const hash = bcrypt.hashSync(password);
+    const hash = bcrypt.hashSync(password, 8);
 
     Users.insert({username, password: hash})
         .then(user => res.status(201).json(user))
@@ -26,27 +26,22 @@ router.post('/', (req, res) => {
         });
 });
 
-router.post('/login', auth, (req, res) => {
-    const { username } = req.body;
-
-    Users.getByUsername(username)
-        .then(user => {
-            res.status(200).json({message: `Welcome, ${user.username}`});
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: "Could not log in"});
-        });
-});
-
-// custom middleware
-function auth(req, res, next) {
+router.post('/login', (req, res) => {
     const {username, password} = req.body;
     if(username && password) {
         Users.getByUsername(username)
             .then(user => {
                 if(user && bcrypt.compareSync(password, user.password)) {
-                    next();
+                    req.session.user = user;
+                    // next();
+                    Users.getByUsername(username)
+                        .then(user => {
+                            res.status(200).json({message: `Welcome, ${user.username}`});
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({error: "Could not log in"});
+                        });
                 } else {
                     res.status(401).json({message: "Invalid credentials"});
                 }
@@ -57,6 +52,45 @@ function auth(req, res, next) {
             });
     } else {
         res.status(400).json({message: "Must provide credentials"});
+    }
+
+});
+
+// custom middleware
+function auth(req, res, next) {
+    const {username, password} = req.body;
+    if(username && password) {
+        Users.getByUsername(username)
+            .then(user => {
+                if(user && bcrypt.compareSync(password, user.password)) {
+                    req.session.user = user;
+                    // next();
+                    Users.getByUsername(username)
+                        .then(user => {
+                            res.status(200).json({message: `Welcome, ${user.username}`});
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({error: "Could not log in"});
+                        });
+                } else {
+                    res.status(401).json({message: "Invalid credentials"});
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error: "Could not retrieve user"});
+            });
+    } else {
+        res.status(400).json({message: "Must provide credentials"});
+    }
+}
+
+function isLoggedIn(req, res, next) {
+    if(req.session && req.session.user) {
+        next();
+    } else {
+        res.status(401).json({message: "You shall not pass"})
     }
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const db = require('./data/db.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use(session({
+    name: 'llama',
+    secret: process.env.SESSION_SECRERT || 'this is a secret',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        knex: db,
+        tablename: 'knexsessions',
+        createtable: true,
+        clearInterval: 1000 * 60 * 30
+    })
+}))
 
 const userRouter = require('./data/users/users-router.js');
 server.use('/users', userRouter);
